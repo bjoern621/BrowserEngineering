34f285f3418d87056f6a017295e0cb5499c72cc9
Björn <41452212+bjoern621@users.noreply.github.com>
2025-04-21 21:46:28 +0200

    Added url as run parameter

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-from .url import URL
+import sys
+from url import URL
 
 
 def main():
     print("Hello, Browser Engineering!")
 
-    url = "http://example.com"
+    url = sys.argv[1] if len(sys.argv) > 1 else "http://example.com/"
+
     load(URL(url))
 
 
