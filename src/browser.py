695f48d12d989292a09e72b65f62c88d95f9af37
Björn <41452212+bjoern621@users.noreply.github.com>
2025-05-17 19:54:00 +0200

    exercise 2-3 Resizing

--- src/browser.py
+++ src/browser.py
@@ -3,8 +3,8 @@ from typing import List, Tuple
 
 from url import URL
 
-WIDTH = 800
-HEIGHT = 600
+INITIAL_WIDTH = 800
+INITIAL_HEIGHT = 600
 
 HSTEP, VSTEP = 13, 18
 
@@ -14,8 +14,12 @@ SCROLL_STEP = 100
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
-        self.canvas.pack()  # The pack() method is used to add the canvas widget to the window's layout.
+        self.canvas = tkinter.Canvas(
+            self.window, width=INITIAL_WIDTH, height=INITIAL_HEIGHT
+        )
+        self.canvas.pack(
+            fill=tkinter.BOTH, expand=True
+        )  # The pack() method is used to add the canvas widget to the window's layout.
 
         self.scroll = 0
         self.window.bind("<Up>", lambda e: self.scroll_up())
@@ -24,13 +28,17 @@ class Browser:
             "<MouseWheel>",
             lambda e: self.handle_mouse_wheel(e.delta),
         )
+        self.window.bind("<Configure>", lambda e: self.handle_resize(e.width, e.height))
+
+        self.width = INITIAL_WIDTH
+        self.height = INITIAL_HEIGHT
 
     def load(self, url: URL):
         """Load the URL and display its content in the browser."""
 
         body = url.request()
-        text = lex(body)
-        self.display_list = layout(text)
+        self.text = lex(body)
+        self.display_list = layout(self.text, INITIAL_WIDTH)
         self.draw()
 
     def draw(self):
@@ -40,7 +48,7 @@ class Browser:
 
         for x, y, char in self.display_list:
             # Skip characters that are not in the current view
-            if y > self.scroll + HEIGHT:
+            if y > self.scroll + self.height:
                 continue
             if y + VSTEP < self.scroll:
                 continue
@@ -64,8 +72,19 @@ class Browser:
         else:
             self.scroll_down(abs(delta))
 
+    def handle_resize(self, width: int, height: int):
+        """Handle window resize events. Re-layout and redraw the content."""
 
-def layout(text: str):
+        assert self.text is not None, "Text content is not loaded yet"
+
+        self.width = width
+        self.height = height
+
+        self.display_list = layout(self.text, width)
+        self.draw()
+
+
+def layout(text: str, width: int):
     """Layout the text for display on the canvas. It returns a display list of tuples with x, y coordinates and the character. The coordinates are page coordinates."""
 
     display_list: List[Tuple[float, float, str]] = []
@@ -81,7 +100,7 @@ def layout(text: str):
         display_list.append((cursor_x, cursor_y, char))
         cursor_x += HSTEP
 
-        if cursor_x > WIDTH - HSTEP:
+        if cursor_x > width - HSTEP:
             cursor_x = HSTEP
             cursor_y += VSTEP
 
